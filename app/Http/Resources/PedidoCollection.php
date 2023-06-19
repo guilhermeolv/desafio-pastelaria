d7567ba77234765284598d248b72bdc99fad016d
Guilherme Oliveira <53823943+guilhermeolv@users.noreply.github.com>
2023-06-19 14:31:28 -0300

    feat: envio das collections

--- app/Http/Resources/PedidoCollection.php
+++ app/Http/Resources/PedidoCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ClienteCollection extends ResourceCollection
+class PedidoCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
