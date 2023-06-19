834ca13890daabb8cffd32082fe26a767be903dc
Guilherme Oliveira <53823943+guilhermeolv@users.noreply.github.com>
2023-06-19 17:27:08 -0300

    fix: ClienteController use controller

--- app/Http/Controllers/Api/ClienteController.php
+++ app/Http/Controllers/Api/ClienteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
 use App\Http\Resources\ClienteCollection;
